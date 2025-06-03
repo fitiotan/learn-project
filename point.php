9333010c3ef3b3f2d00033579de49210ff72cb66
fitio <fitiotan@gmail.com>
2025-06-03 14:49:07 +0800

    modified:   point.php

--- point.php
+++ point.php
@@ -351,6 +351,8 @@ function sendMessage() {
 }
 </script>
 
+<script src="js/scripts.js"></script>
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
